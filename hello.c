8fd5ae3c174142c8ca445dbdfa9ac1cd39ba15b8
Christina Sørensen <christina@cafkafk.com>
2023-11-02 15:30:37 +0100

    feat: add something to test for pre-releases
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- hello.c
+++ hello.c
@@ -2,5 +2,5 @@
 
 int main(int argc, char * * argv)
 {
-    printf("The fault, dear Brutus, is not in our flakes, but in our governance, that we aren't moderators.\n");
+    printf("The fault, dear pre-release-Brutus, is not in our flakes, but in our governance, that we aren't moderators.\n");
 }
